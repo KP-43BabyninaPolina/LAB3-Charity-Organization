5a207bd2b54a0748f237b7a09097908e2ca5b0f0
[КП-43] Бабиніна Поліна <Babynina.Polina@LLL.kpi.ua>
2026-05-27 17:11:44 +0300

    CharityDatabase added.

---
+++ app/src/main/java/java/ua/kpi/charity/database/CharityDatabase.java
@@ -0,0 +1,27 @@
+package java.ua.kpi.charity.database;
+
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.ua.kpi.charity.dao.CampaignDao;
+import java.ua.kpi.charity.entity.Campaign;
+
+@Database(entities = {Campaign.class}, version = 1)
+public abstract class CharityDatabase extends RoomDatabase {
+
+    private static CharityDatabase INSTANCE;
+
+    public abstract CampaignDao campaignDao();
+
+
+    public static synchronized CharityDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            CharityDatabase.class, "charity_db")
+                    .build();
+        }
+        return INSTANCE;
+    }
+}
